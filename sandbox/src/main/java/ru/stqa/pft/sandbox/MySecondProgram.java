2c04d7a22414c4b9a52a6471fd135f1d68e4ad6c
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-02-27 02:37:01 +0300

    class point and MySecondProgram finalized
    fixed the Println syntax and method syntax

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
@@ -7,10 +7,6 @@ public class MySecondProgram {
     Point p1 = new Point(1.0,1.0);
     Point p2 = new Point(4.0,5.0);
 
-    System.out.println(distance(p1, p2));
-  }
-
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    System.out.println( p1.distance(p1, p2) );
   }
 }
