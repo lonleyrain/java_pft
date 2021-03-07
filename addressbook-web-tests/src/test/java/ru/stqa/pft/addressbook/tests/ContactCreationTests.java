391b1d75048fae7ba666d6bea582244177b225ce
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-03-07 22:47:31 +0300

    optimized some imports

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,10 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
-import java.util.concurrent.TimeUnit;
+
 
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactCreationTests extends TestBase {
@@ -17,6 +14,5 @@ public class ContactCreationTests extends TestBase {
     submitContactForm();
     returnToHomePage();
   }
-
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,17 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.ContactData;
-
-import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
@@ -23,11 +18,11 @@ public class TestBase {
     app.init();
   }
 
-  protected void initNewContactCreation() {
+  public void initNewContactCreation() {
     wd.findElement(By.linkText("add new")).click();
   }
 
-  protected void fillContactForm(ContactData contactData) {
+  public void fillContactForm(ContactData contactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
     wd.findElement(By.name("firstname")).sendKeys(contactData.getFirst_name());
@@ -45,11 +40,11 @@ public class TestBase {
     wd.findElement(By.name("new_group")).click();
   }
 
-  protected void submitContactForm() {
+  public void submitContactForm() {
     wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
   }
 
-  protected void returnToHomePage() {
+  public void returnToHomePage() {
     wd.findElement(By.linkText("home page")).click();
   }
 
@@ -58,21 +53,4 @@ public class TestBase {
     app.stop();
   }
   
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 }
