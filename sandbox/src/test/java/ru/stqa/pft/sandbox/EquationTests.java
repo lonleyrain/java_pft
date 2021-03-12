8348a80dd8162eaf5fec7154d0f2a328c0e93eca
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-03-12 23:24:28 +0300

    Equation Tests added

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -0,0 +1,39 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EquationTests {
+
+  // тесты для ситуации , когда уравнение не имеет решений
+
+  @Test
+  public void test0(){
+
+    Equation e = new Equation(1,1,1);
+    Assert.assertEquals(e.rootNumber(), 0);
+
+  }
+
+  // тесты для ситуации , когда уравнение имеет 1 решение
+
+  @Test
+  public void test1(){
+
+    Equation e = new Equation(1,2,1);
+    Assert.assertEquals(e.rootNumber(), 1);
+
+  }
+
+  // тесты для ситуации , когда уравнение имеет 2 решения
+
+  @Test
+  public void test2(){
+
+    Equation e = new Equation(1,5,6);
+    Assert.assertEquals(e.rootNumber(), 2);
+
+  }
+
+
+}
