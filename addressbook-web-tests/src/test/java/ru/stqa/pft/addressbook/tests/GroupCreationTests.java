dda7c3830a59920bb0323d3ce03fd9cbee382b0b
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-03-14 11:59:31 +0300

    GroupCreationTest enhacements (default values)
    Added if clause, which helps to check if we need to fill in fileds in form: if values we send are not null -> then we send the text provided (and clear the field before). If the values we send are null -> then not do anything (leave defaul values for the form)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -11,7 +11,7 @@ public class GroupCreationTests extends TestBase {
 
     app.getNavigationHelper().goToGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
     app.getGroupHelper().submitGroupCreationForm();
     app.getGroupHelper().returnToGroupPage();
   }
