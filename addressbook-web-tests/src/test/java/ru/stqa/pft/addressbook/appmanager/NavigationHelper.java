7b8b295364b53a63f58e3d3b969c05523f6cd999
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-03-14 18:03:26 +0300

    Page navigation optimization

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,10 +10,26 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void goToGroupPage() {
+
+    if (isElementPresent(By.tagName("h1"))
+            &&  wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            &&  isElementPresent(By.name("new"))) {
+      return;
+    }
+
     click(By.linkText("groups"));
+
   }
 
   public void returnToHomePage() {
+
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home page"));
   }
 }
+
+/*&& означаеет "и" в моменте перечисления условий
+* || означает "или"
+* */
