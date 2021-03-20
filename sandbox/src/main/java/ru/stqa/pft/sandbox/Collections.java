abfb97aa7199e1ea3332f6f01c7687fe1569d10d
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-03-20 11:59:43 +0300

    Collections class added

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+
+  public static void main(String[] args) {
+
+    String[] langs = {"java", "C#", "Python", "PHP"};
+
+    List<String> languages = Arrays.asList("java", "C#", "Python", "PHP");
+
+
+    for (String l : languages) {
+      System.out.println("I want to learn " + l);
+    }
+
+
+  }
+
+}
