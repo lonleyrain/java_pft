baebde054bda888cb8afcf8321cef2cc69849f34
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-03-20 21:58:18 +0300

    added contact.remove(0) to remove table header

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -1,13 +1,12 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -93,8 +92,9 @@ public class ContactHelper extends HelperBase {
 
       ContactData contact = new ContactData(first_name, last_name, phone_number, null, null);
       contacts.add(contact);
+      contacts.remove(0);
     }
-    //contacts.remove(0);
+
 
     return contacts;
 
