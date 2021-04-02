24fbc038344286e9555ad7f8490f823e9082cf93
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-04-02 18:53:48 +0300

    XStream integration added, Data provider is now supporting XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -3,7 +3,9 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -19,6 +21,9 @@ public class ContactDataGenerator {
 
   @Parameter (names = "-f", description = "Target file")
   public String file;
+
+  @Parameter (names = "-d", description = "Data format")
+  public String format;
 
   public static void main(String[] args) throws IOException {
 
@@ -36,10 +41,26 @@ public class ContactDataGenerator {
 
   private void run() throws IOException {
     List<ContactData> contacts = generateContacts(count);
-    save(contacts, new File(file));
+    if (format.equals("csv")) {
+      saveAsCSV(contacts, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXML(contacts, new File(file));
+    } else {
+      System.out.println("Unrecognized format " + format);
+    }
+
   }
 
-  private static void save(List<ContactData> contacts, File file) throws IOException {
+  private void saveAsXML(List<ContactData> contacts, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    String xml = xstream.toXML(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+  private static void saveAsCSV(List<ContactData> contacts, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (ContactData contact : contacts) {
       writer.write(String.format("%s;%s;%s;%s;%s\n",
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
@@ -11,6 +12,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -32,22 +34,19 @@ public class ContactCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validContacts() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();
-
     BufferedReader reader =
-            new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+            new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line != null ) {
-      String[] split = line.split(";");
-      list.add(new Object[] {new ContactData()
-              .withFirst_name(split[0])
-              .withLast_name(split[1])
-              .withPhone_number(split[2])
-              .withEmail(split[3])
-              .withGroup_name(split[4])});
+      xml+=line;
       line = reader.readLine();
     }
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>)xstream.fromXML(xml);
+    return contacts.stream().map((c)-> new Object[] {c}).collect(Collectors.toList()).iterator();
+
   }
   /*.withPhoto(new File("src/test/resources/stru.png")*/
   @Test (dataProvider = "validContacts")
