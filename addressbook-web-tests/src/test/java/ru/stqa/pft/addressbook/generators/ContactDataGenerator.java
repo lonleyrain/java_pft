9252f43fceae046921340f425c005b120b07f794
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-04-02 22:48:33 +0300

    added try () {} method to close writers/readers

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -58,31 +58,31 @@ public class ContactDataGenerator {
   private void saveAsJSON(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(json);
+    }
   }
 
   private void saveAsXML(List<ContactData> contacts, File file) throws IOException {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file);) {
+      writer.write(xml);
+    }
   }
 
   private static void saveAsCSV(List<ContactData> contacts, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    for (ContactData contact : contacts) {
-      writer.write(String.format("%s;%s;%s;%s;%s\n",
-              contact.getFirst_name(),
-              contact.getLast_name(),
-              contact.getPhone_number(),
-              contact.getEmail(),
-              contact.getGroup_name()));
+    try (Writer writer = new FileWriter(file)) {
+      for (ContactData contact : contacts) {
+        writer.write(String.format("%s;%s;%s;%s;%s\n",
+                contact.getFirst_name(),
+                contact.getLast_name(),
+                contact.getPhone_number(),
+                contact.getEmail(),
+                contact.getGroup_name()));
+      }
     }
-    writer.close();
   }
 
   private static List<ContactData> generateContacts(int count) {
