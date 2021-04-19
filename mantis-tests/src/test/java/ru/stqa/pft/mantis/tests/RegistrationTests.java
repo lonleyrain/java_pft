fb63c8f58894d3737e2e0d218343465047143a5c
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-04-19 23:37:51 +0300

    added MailHelper, MailMessage Model

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,8 +1,15 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class RegistrationTests extends TestBase {
+
+  @BeforeMethod
+  public void startMailServer() {
+    app.mail().start();
+  }
 
   @Test
 
@@ -11,5 +18,11 @@ public class RegistrationTests extends TestBase {
     app.registration().start("user1", "user1@localhost.localdomain");
 
 
+  }
+
+  @AfterMethod (alwaysRun = true)
+
+  public void stopMailServer() {
+    app.mail().stop();
   }
 }
