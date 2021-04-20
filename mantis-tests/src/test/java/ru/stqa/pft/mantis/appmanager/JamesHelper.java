ab8e4387b4e23d4b6c8f54a63614f633d593e0ec
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-04-21 00:13:02 +0300

    added more handwriting

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/JamesHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/JamesHelper.java
@@ -42,4 +42,63 @@ public class JamesHelper {
 
   }
 
+  public void deleteUser(String name) {
+    initTelnetSession();
+    write("deluser " + name);
+    String result = readUntil("User " + name + " deleted");
+    closeTelnetSession();
+  }
+
+  private void initTelnetSession() {
+    mailserver = app.getProperty("mailserver.host");
+    int port = Integer.parseInt(app.getProperty("mailserver.port"));
+    String login = app.getProperty("mailserver.adminlogin");
+    String password = app.getProperty("mailserver.adminpassword");
+
+    try {
+      telnet.connect(mailserver, port);
+      in = telnet.getInputStream();
+      out = new PrintStream(telnet.getOutputStream());
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    // first attempt
+    readUntil("Login id:");
+    write("");
+    readUntil("Password");
+    write("");
+
+    //second
+    readUntil("Login id:");
+    write("");
+    readUntil("Password");
+    write("");
+
+    //read welcome message
+    readUntil("Welcome ", +login + ". HELP for a list of commands");
+  }
+
+  private String readUntil (String pattern) {
+    try {
+      char lastChar = pattern.charAt(pattern.length() - 1);
+      StringBuffer sb = new StringBuffer();
+      char ch = (char) in.read();
+      while (true) {
+        System.out.println(ch)
+        sb.append(ch);
+        if (ch == lastChar) {
+          if (sb.toString().endsWith(pattern)){
+            return sb.toString();
+          }
+        }
+        ch = (char) in.read();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
 }
