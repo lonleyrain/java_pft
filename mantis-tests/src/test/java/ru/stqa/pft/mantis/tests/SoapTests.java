54aeaf960552bb35f2e1cec566cda0f1e505e6c1
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-04-25 12:30:41 +0300

    addIssue test added

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -1,11 +1,14 @@
 package ru.stqa.pft.mantis.tests;
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
 import java.util.Set;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class SoapTests extends TestBase {
 
@@ -19,4 +22,20 @@ public class SoapTests extends TestBase {
       System.out.println(project.getName());
     }
   }
+
+  @Test
+
+  public void testCreateIssue() throws MalformedURLException, ServiceException, RemoteException {
+    Set<Project> projects = app.soapHelper().getProjects();
+
+    Issue issue = new Issue()
+            .withSummary("Test issue")
+            .withDescription("test issue description")
+            .withProject(projects.iterator().next());
+
+    Issue created_issue = app.soapHelper().addIssue(issue);
+    assertEquals(issue.getSummary(), created_issue.getSummary());
+
+  }
+
 }
