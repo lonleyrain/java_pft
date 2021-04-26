c9d27a351e0daeefa77dfacd853aee43fcc3dd9f
Anastasiya Semiankevich <anastasija.ig@gmail.com>
2021-04-26 20:04:54 +0300

    bugify Test added

--- soap-sample/src/test/java/ru/stqa/pft/soap/GeoIPServiceTests.java
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIPServiceTests.java
@@ -4,6 +4,8 @@ import com.lavasoft.GeoIPService;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.nio.charset.StandardCharsets;
+
 import static org.testng.Assert.assertEquals;
 
 public class GeoIPServiceTests {
@@ -12,8 +14,8 @@ public class GeoIPServiceTests {
 
   public void testGeoIPService() {
 
-    String isoCountryName = new GeoIPService().getGeoIPServiceSoap12().getCountryISO2ByName("USA");
-    assertEquals(isoCountryName, "<GeoIP><Country>US</Country></GeoIP>");
+    String ipLocation20 = new GeoIPService().getGeoIPServiceSoap12().getIpLocation20("46.53.246.20");
+    assertEquals(ipLocation20, "<GeoIP><Country>BY</Country><State>04</State></GeoIP>");
 
 
   }
